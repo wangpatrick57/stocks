37625b8db3f6d18009410a09d2c35e6a13f1b644
Patrick Wang <patrickwang@Patricks-MacBook-Pro.local>
2021-03-08 19:26:09 -0800

    90x speed increase and geo mean instead of arith mean

--- main.py
+++ main.py
@@ -6,19 +6,25 @@ import statistics
 
 START_AMOUNT = 10000
 EARLIEST_DATE_STR = '1927-12-30'
-LATEST_DATE_STR = '2020-12-30'
+LATEST_DATE_STR = '2020-12-31'
 
-def print_list_analytics(annual_return_list):
-    mean = statistics.mean(annual_return_list)
-    median = statistics.median(annual_return_list)
-    stdev = statistics.stdev(annual_return_list)
-    list_min = min(annual_return_list)
-    list_max = max(annual_return_list)
-    print(f'mean: {mean}')
-    print(f'median: {median}')
-    print(f'stdev: {stdev}')
-    print(f'min: {list_min}')
-    print(f'max: {list_max}')
+def calculate_annual_return(start_amount, end_amount, years):
+    return ((end_amount / start_amount) ** (1 / years) - 1) * 100
+
+def output_str_format(end_amount, start_amount, years):
+    return f'${end_amount:.0f} –– {calculate_annual_return(start_amount, end_amount, years):.2f}%'
+
+def print_list_analytics(end_amount_list, start_amount, years):
+    mean = statistics.mean(end_amount_list)
+    median = statistics.median(end_amount_list)
+    stdev = statistics.stdev(end_amount_list)
+    list_min = min(end_amount_list)
+    list_max = max(end_amount_list)
+    print(f'mean: {output_str_format(mean, start_amount, years)}')
+    print(f'median: {output_str_format(median, start_amount, years)}')
+    print(f'stdev: {output_str_format(stdev, start_amount, years)}')
+    print(f'min: {output_str_format(list_min, start_amount, years)}')
+    print(f'max: {output_str_format(list_max, start_amount, years)}')
 
 def print_list_title(investment_months, window_years, tax_rate):
     print(f'{investment_months} months | {window_years} years | {tax_rate * 100}% tax')
@@ -31,12 +37,12 @@ def main():
 
     monkey = Monkey(EARLIEST_DATE_STR, LATEST_DATE_STR, START_AMOUNT, stock_data, dividend_data)
 
-    for investment_months in [1]:
+    for investment_months in [1, 3, 6, 12, 24, 36]:
         for window_years in [30]:
-            for tax_rate in [1]:
-                annual_return_list = monkey.get_annual_return_list(investment_months, window_years, tax_rate)
+            for tax_rate in [0.4]:
+                end_amount_list = monkey.get_end_amount_list(investment_months, window_years, tax_rate)
                 print_list_title(investment_months, window_years, tax_rate)
-                print_list_analytics(annual_return_list)
+                print_list_analytics(end_amount_list, START_AMOUNT, window_years)
                 print()
 
 if __name__ == '__main__':
--- processing_helpers.py
+++ processing_helpers.py
@@ -2,11 +2,49 @@ from datetime import date
 from datetime import timedelta
 import math
 
-DAYS_IN_YEAR = 365
-DAYS_IN_MONTH = 30
+def get_current_quarter(current_date):
+    if current_date.month <= 3:
+        return 0
+    elif current_date.month <= 6:
+        return 1
+    elif current_date.month <= 9:
+        return 2
+    elif current_date.month <= 12:
+        return 3
+
+def get_start_month_date(base_date):
+    if base_date.day == 1:
+        return base_date
+    else:
+        return get_next_month_date(base_date)
+
+def get_next_month_date(base_date):
+    if base_date.month == 12:
+        return date(base_date.year + 1, 1, 1)
+    else:
+        return date(base_date.year, base_date.month + 1, 1)
+
+def get_start_quarter_date(base_date):
+    if base_date.month >= 10:
+        return date(base_date.year + 1, 1, 1)
+    else:
+        if base_date.month <= 3:
+            quarter_month = 4
+        elif base_date.month <= 6:
+            quarter_month = 7
+        elif base_date.month <= 9:
+            quarter_month = 10
+
+        return date(base_date.year, quarter_month, 1)
+
+def get_next_quarter_date(base_date):
+    if base_date.month == 10:
+        return date(base_date.year + 1, 1, 1)
+    else:
+        return date(base_date.year, base_date.month + 3, 1)
 
 class Monkey:
-    DAY_FREQUENCY = 30
+    DAY_FREQUENCY = 1
 
     def __init__(self, earliest_date_str, latest_date_str, start_amount, stock_data, dividend_data):
         self._earliest_date = date.fromisoformat(earliest_date_str)
@@ -15,89 +53,76 @@ class Monkey:
         self._stock_data = stock_data
         self._dividend_data = dividend_data
 
-    def get_annual_return_list(self, investment_months, window_years, tax_rate):
-        annual_return_list = []
+    def get_end_amount_list(self, investment_months, window_years, tax_rate):
+        end_amount_list = []
         current_date = self._earliest_date
-        current_end_date = current_date + timedelta(days = window_years * DAYS_IN_YEAR)
+        current_end_date = date(current_date.year + window_years, current_date.month, current_date.day)
 
         while current_end_date <= self._latest_date:
-            annual_return_list.append(self._get_annual_return(investment_months, window_years, tax_rate, current_date))
+            end_amount_list.append(self._get_end_amount(investment_months, window_years, tax_rate, current_date))
             current_date += timedelta(days = Monkey.DAY_FREQUENCY)
-            current_end_date = current_date + timedelta(days = window_years * DAYS_IN_YEAR)
 
-        return annual_return_list
+            if current_date.month == 2 and current_date.day == 29:
+                current_date += timedelta(days = 1)
 
-    def _get_annual_return(self, investment_months, window_years, tax_rate, start_date):
-        current_date = start_date
-        end_date = start_date + timedelta(days = window_years * DAYS_IN_YEAR)
+            current_end_date = date(current_date.year + window_years, current_date.month, current_date.day)
+
+        return end_amount_list
+
+    def _get_end_amount(self, investment_months, window_years, tax_rate, start_date):
+        end_date = date(start_date.year + window_years, start_date.month, start_date.day)
         current_price = -1
         owned_shares = 0
-        days_since_last_investment = 0
         months_invested = 0
-        last_dividend_quarter = Monkey.get_current_quarter(current_date)
+        last_dividend_quarter = get_current_quarter(start_date - timedelta(days = 1))
+        jump_dates = []
 
-        while current_date <= end_date:
+        # add months
+        for i in range(investment_months):
+            if i == 0:
+                jump_dates.append(get_start_month_date(start_date))
+            else:
+                jump_dates.append(get_next_month_date(jump_dates[-1]))
+
+        # add dividends
+        curr_dividend_date = get_start_quarter_date(jump_dates[-1])
+
+        while curr_dividend_date < end_date:
+            jump_dates.append(curr_dividend_date)
+            curr_dividend_date = get_next_quarter_date(curr_dividend_date)
+
+        for current_date in jump_dates:
             current_date_str = current_date.isoformat()
 
-            if current_date_str in self._stock_data:
-                current_price = self._stock_data[current_date_str]['adj_close']
-                current_quarter = Monkey.get_current_quarter(current_date)
+            while current_date_str not in self._stock_data and current_date <= self._latest_date:
+                current_date += timedelta(days = 1)
+                current_date_str = current_date.isoformat()
 
-                # invest initial if it's the first day or it's been a month
-                if months_invested < investment_months and (owned_shares == 0 or days_since_last_investment > DAYS_IN_MONTH):
-                    money_to_invest = self._start_amount / investment_months
-                    owned_shares += money_to_invest / current_price
+            if current_date > self._latest_date:
+                break
 
-                    months_invested += 1
-                    days_since_last_investment = 0
+            current_price = self._stock_data[current_date_str]['adj_close']
+            current_quarter = get_current_quarter(current_date)
 
-                # reinvest dividends if it's a new quarter
-                if current_quarter != last_dividend_quarter:
-                    money_to_invest = self._dividend_data[current_date.year] / 4 * owned_shares * current_price * (1 - tax_rate)
-                    owned_shares += money_to_invest / current_price
-                    last_dividend_quarter = current_quarter
+            # invest initial if that hasn't happened enough yet
+            if months_invested < investment_months:
+                money_to_invest = self._start_amount / investment_months
+                owned_shares += money_to_invest / current_price
 
-            days_since_last_investment += 1
-            current_date += timedelta(days = 1)
+                months_invested += 1
+                days_since_last_investment = 0
+
+            # reinvest dividends if it's a new quarter
+            if current_quarter != last_dividend_quarter:
+                dividend_year_to_use = current_date.year
+
+                while dividend_year_to_use not in self._dividend_data:
+                    dividend_year_to_use -= 1
+
+                money_to_invest = self._dividend_data[dividend_year_to_use] / 4 * owned_shares * current_price * (1 - tax_rate)
+                owned_shares += money_to_invest / current_price
+                last_dividend_quarter = current_quarter
 
         end_amount = owned_shares * current_price
-        annual_return = self._calculate_annual_return(end_amount, window_years)
 
-        if annual_return < -5:
-            print(start_date, annual_return)
-
-        return annual_return
-
-    def _calculate_annual_return(self, end_amount, years):
-        return ((end_amount / self._start_amount) ** (1 / years) - 1) * 100
-
-    @staticmethod
-    def get_current_quarter(current_date):
-        if current_date.month < 3:
-            return 0
-        elif current_date.month == 3:
-            if current_date.day <= 20:
-                return 0
-            else:
-                return 1
-        elif current_date.month < 6:
-            return 1
-        elif current_date.month == 6:
-            if current_date.day <= 20:
-                return 1
-            else:
-                return 2
-        elif current_date.month < 9:
-            return 2
-        elif current_date.month == 9:
-            if current_date.day <= 20:
-                return 2
-            else:
-                return 3
-        elif current_date.month < 12:
-            return 3
-        elif current_date.month == 12:
-            if current_date.day <= 20:
-                return 3
-            else:
-                return 0
+        return end_amount
